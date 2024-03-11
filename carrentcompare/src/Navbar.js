5294c62efd13d0f0064cc2ca603bc9f6ecd9f76c
bouheddadj <bnasr3369@gmail.com>
2024-03-11 08:39:19 +0100

    Préparation

---
+++ carrentcompare/src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        {/*<h1>Car Rent Compare</h1>*/}
+                        <div className="box"></div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
+
