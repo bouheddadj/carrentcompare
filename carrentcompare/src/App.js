e6c5df52f8066a47f183b0320711eb4555800fcc
bouheddadj <bnasr3369@gmail.com>
2024-03-12 13:46:28 +0100

    Fixing npm start errors

--- carrentcompare/src/App.js
+++ carrentcompare/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react'
+import { useEffect } from 'react';
 import Navbar from './Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
+
+    <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <div className='box'></div>
+          </div>
+          <div className='col'>
+            <div className='box'></div>
+          </div>
+          <div className='col'>
+            <div className='box'></div>
+          </div>
+        </div>
     </div>
+
   );
 }
 
--- carrentcompare/src/Navbar.js
+++ carrentcompare/src/Navbar.js
@@ -1,15 +1,9 @@
+import React from 'react'
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        {/*<h1>Car Rent Compare</h1>*/}
-                        <div className="box"></div>
-                    </div>
-                </div>
-            </div>
-        </nav>
+
+        {}
+
     );
 }
  
